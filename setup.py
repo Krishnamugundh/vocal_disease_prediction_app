b706d31656d716f2c3e9d3d1ee7369aa594c6735
Krishnamugundh P <90767567+Krishnamugundh@users.noreply.github.com>
2024-06-18 21:44:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 setuptools.setup(
     name='audio_detections',
     version='0.0.2',
-    description='Post-installation script for pywin32',
+    description='Audio description for voice classification',
     author='mugu',
-    author_email='xyz@gmail.com',
+    author_email='forbusinesssuseonly1986@gmail.com',
     py_modules=['pywin32_postinstall'],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
